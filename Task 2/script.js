94bf522c2bd83382ad50845b4a0cc5e27e892e7b
GintareSlusnyte <gintareslusnyte@gmail.com>
2023-04-11 11:42:39 +0300

    Updated Task2

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,3 +3,12 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+const btnElement = document.querySelector('#btn__element');
+const btnState = document.querySelector('#btn__state');
+let count = 0;
+
+btnElement.addEventListener('click', () => {
+  count++;
+  btnState.textContent = count;
+});
--- Task 3/script.js
+++ Task 3/script.js
@@ -11,3 +11,45 @@ Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų 
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+
+import Post from './Post.js';
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    // console.log(data);
+    data.forEach(user => {
+      document.querySelector('#test').innerHTML += `<h1>${user.name}</h1>`
+    })
+  })
+  .catch(err => {
+    console.log(err.message);
+  })
+  .finally(() => {
+    // console.log('end');
+  })
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(res => res.json())
+  .then(posts => {
+    const allPosts = document.querySelector('#allPosts');
+    posts.forEach(post => {
+      // console.log(post);
+      const newPost = new Post(post);
+      allPosts.appendChild(newPost);
+    });
+  });
+
+
+
+  fetch('https://api.github.com/users')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    data.forEach(user => {
+        document.querySelector('#output').innerHTML += 
+    })
+  })
